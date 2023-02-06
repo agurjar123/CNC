e7c0464aae76ac2dc3544e6974f61163fb138d10
agurjar123 <63985310+agurjar123@users.noreply.github.com>
2023-02-05 19:42:58 -0800

    Add files via upload

--- visualize.m
+++ visualize.m
@@ -4,12 +4,16 @@ gcp;
 name = 'm123.3_d211129_s03_magnetFront_test_2p_00001.tif';
 
 tic; Y = read_file(name); toc; % read the file (optional, you can also pass the path in the function instead of Y)
-Y = single(Y);                 % convert to single precision 
-T = size(Y,ndims(Y));
-Y = Y - min(Y(:));
+%Y = single(Y);                 % convert to single precision 
+%T = size(Y,ndims(Y));
+%Y = Y - min(Y(:));
+disp(size(Y));
+out = Y(:,:,1:100);
+imagesc(mean(out,3));
+colorbar;
 %% set parameters (first try out rigid motion correction)
 
-options_rigid = NoRMCorreSetParms('d1',size(Y,1),'d2',size(Y,2),'bin_width',200,'max_shift',15,'us_fac',50,'init_batch',200);
+options_rigid = NoRMCorreSetParms('d1',size(Y,1),'d2',size(Y,2),'bin_width',200,'max_shift',15,'us_fac',50,'init_batch',200, 'make_avi', true);
 
 %% perform motion correction
 tic; [M1,shifts1,template1,options_rigid] = normcorre(Y,options_rigid); toc
@@ -39,35 +43,35 @@ figure;
     subplot(2,3,6); scatter(cM1,cM2); hold on; plot([0.9*min(cY),1.05*max(cM1)],[0.9*min(cY),1.05*max(cM1)],'--r'); axis square;
         xlabel('rigid corrected','fontsize',14,'fontweight','bold'); ylabel('non-rigid corrected','fontsize',14,'fontweight','bold');
     linkaxes([ax1,ax2,ax3],'xy')
-%% plot shifts        
-
-shifts_r = squeeze(cat(3,shifts1(:).shifts));
-shifts_nr = cat(ndims(shifts2(1).shifts)+1,shifts2(:).shifts);
-shifts_nr = reshape(shifts_nr,[],ndims(Y)-1,T);
-shifts_x = squeeze(shifts_nr(:,1,:))';
-shifts_y = squeeze(shifts_nr(:,2,:))';
-
-patch_id = 1:size(shifts_x,2);
-str = strtrim(cellstr(int2str(patch_id.')));
-str = cellfun(@(x) ['patch # ',x],str,'un',0);
-
-figure;
-    ax1 = subplot(311); plot(1:T,cY,1:T,cM1,1:T,cM2); legend('raw data','rigid','non-rigid'); title('correlation coefficients','fontsize',14,'fontweight','bold')
-            set(gca,'Xtick',[])
-    ax2 = subplot(312); plot(shifts_x); hold on; plot(shifts_r(:,1),'--k','linewidth',2); title('displacements along x','fontsize',14,'fontweight','bold')
-            set(gca,'Xtick',[])
-    ax3 = subplot(313); plot(shifts_y); hold on; plot(shifts_r(:,2),'--k','linewidth',2); title('displacements along y','fontsize',14,'fontweight','bold')
-            xlabel('timestep','fontsize',14,'fontweight','bold')
-    linkaxes([ax1,ax2,ax3],'x')
-
-    disp(1:T);
-    disp(cY);
-    disp(cM1);
-    disp(cM2);
-    disp(shifts_x);
-    disp(shifts_r(:,1))
-    disp(shifts_y);
-    disp(shifts_r(:,2))
+% %% plot shifts        
+% 
+% shifts_r = squeeze(cat(3,shifts1(:).shifts));
+% shifts_nr = cat(ndims(shifts2(1).shifts)+1,shifts2(:).shifts);
+% shifts_nr = reshape(shifts_nr,[],ndims(Y)-1,T);
+% shifts_x = squeeze(shifts_nr(:,1,:))';
+% shifts_y = squeeze(shifts_nr(:,2,:))';
+% 
+% patch_id = 1:size(shifts_x,2);
+% str = strtrim(cellstr(int2str(patch_id.')));
+% str = cellfun(@(x) ['patch # ',x],str,'un',0);
+% 
+% figure;
+%     ax1 = subplot(311); plot(1:T,cY,1:T,cM1,1:T,cM2); legend('raw data','rigid','non-rigid'); title('correlation coefficients','fontsize',14,'fontweight','bold')
+%             set(gca,'Xtick',[])
+%     ax2 = subplot(312); plot(shifts_x); hold on; plot(shifts_r(:,1),'--k','linewidth',2); title('displacements along x','fontsize',14,'fontweight','bold')
+%             set(gca,'Xtick',[])
+%     ax3 = subplot(313); plot(shifts_y); hold on; plot(shifts_r(:,2),'--k','linewidth',2); title('displacements along y','fontsize',14,'fontweight','bold')
+%             xlabel('timestep','fontsize',14,'fontweight','bold')
+%     linkaxes([ax1,ax2,ax3],'x')
+% 
+%     disp(1:T);
+%     disp(cY);
+%     disp(cM1);
+%     disp(cM2);
+%     disp(shifts_x);
+%     disp(shifts_r(:,1))
+%     disp(shifts_y);
+%     disp(shifts_r(:,2))
 %% plot a movie with the results
 
 figure;
--- visualizeTiff.py
+++ visualizeTiff.py
@@ -1,13 +1,1 @@
-import numpy as np
-import struct
-from osgeo import gdal
-from osgeo import ogr
-from osgeo import osr
-from osgeo import gdal_array
-from osgeo.gdalconst import *
-import matplotlib.pyplot as plt
-
-cube = gdal.Open(filename)
-bnd1 = cube.GetRasterBand(29)
-bnd2 = cube.GetRasterBand(19)
-bnd3 = cube.GetRasterBand(9)
+import image
