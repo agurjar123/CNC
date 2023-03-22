3eb7d32b1049a9658fc2d38d16f0f57eccf4bf78
agurjar123 <63985310+agurjar123@users.noreply.github.com>
2023-03-21 17:35:54 -0700

    Add files via upload
    The most recent extract to raster plot pipeline that uses H5 files. Dependencies include normecorre and extract

---
+++ Magnet analysis/extract.m
@@ -0,0 +1,49 @@
+%% Start the main pipeline
+% v = VideoReader('motion_corrected.avi');
+% numFrames = v.NumFrames;
+% for i = 1:numFrames
+%     I = read(v,i);
+%     imshow(I)
+%     pause(0.5)
+% end
+fileFolder = 'corrected.h5';
+
+first_image = h5read(fileFolder,'/mov');
+[W,H] = size(first_image);
+D = numel(fileFolder);
+M = zeros(W,H,D);
+M(:,:,1) = first_image;
+for i = 2:D
+    M(:,:,i) = h5read(fileFolder,i);
+    % uncomment next line for seeing the reading progress
+    % disp(string(i*100.0/D) + "%");
+end
+disp(size(M))
+config =[];
+config = get_defaults(config);
+
+config.avg_cell_radius=200;
+config.num_partitions_x=1;
+config.num_partitions_y=1;
+
+% change these as needed
+config.cellfind_min_snr = 0;
+config.thresholds.T_min_snr=1;
+
+%M = 'm123.3_d211129_s03_magnetFront_test_2p_00001.tif';
+
+
+% Add more partitions as needed for the RAM memory. As a rule of thumb, you want to partition the movie such that partitioned movie memory is 1/4th of RAM memory.
+config.num_partitions_x=2;
+config.num_partitions_y=2;
+
+config.max_iter=0;
+
+% If you sorted, make sure that S_in is the sorted cell filters coming from cell check above!
+
+
+output=extractor(M,config);
+save('extract_full_sorted.mat','output','-v7.3');
+
+
+
